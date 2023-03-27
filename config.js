81b5feb79e8444af33ca37327271f8fa407981ab
Tomaz Bontempo <tomaz.bontempo@gmail.com>
2023-03-27 16:29:54 -0300

    changing port

---
+++ config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  user: "guiadoconsumidorinteligente@gmail.com",
+  clientId:
+    "992327083265-n5rdb0nmta7jbttioe5f07lgtgt0tk1v.apps.googleusercontent.com",
+  clientSecret: "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",
+  refreshToken:
+    "1//04Z8v-ebLzAeKCgYIARAAGAQSNwF-L9IrH3CW_2xRdQsrCGSjW0LfISjbCvsMtvnmrnMZSuhNLcV1OVYrTKyk6hjPzVgcbYqAVsQ",
+};
