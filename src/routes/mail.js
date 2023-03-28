3fd37336ccc0253519c70e202e541c62036ac464
Tomaz Bontempo <tomaz.bontempo@gmail.com>
2023-03-28 17:27:51 -0300

    Added health check route

--- src/routes/mail.js
+++ src/routes/mail.js
@@ -2,6 +2,20 @@ const express = require("express");
 const router = express.Router();
 const sendEmail = require("../../sendEmail");
 
+router.get("/", async (_req, res, _next) => {
+  const healthcheck = {
+    uptime: process.uptime(),
+    message: "OK",
+    timestamp: Date.now(),
+  };
+  try {
+    res.send(healthcheck);
+  } catch (error) {
+    healthcheck.message = error;
+    res.status(503).send();
+  }
+});
+
 router.post("/", (req, res) => {
   console.log("Recebendo valores");
   const email = req.body.email;
