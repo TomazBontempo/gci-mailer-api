21e029110b688635187d26ce1644dcf4f4f75648
Tomaz Bontempo <tomaz.bontempo@gmail.com>
2023-03-29 01:40:02 -0300

    testing stuff

--- app.js
+++ app.js
@@ -4,10 +4,16 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.use("/health", require("./routes/health"));
+app.get("/", (req, res) => {
+  res.json({ msg: "Online" });
+});
+
+app.get("/test", (req, res) => {
+  res.json({ msg: "test successful" });
+});
 
 app.use("/mails", mailRouter);
 
-app.listen(8080, () => {
+app.listen(3000, () => {
   console.log("Server online");
 });
