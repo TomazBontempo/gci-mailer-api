628757ceb23e9b0fedbdca3f267bcf40e12f29be
Tomaz Bontempo <tomaz.bontempo@gmail.com>
2023-04-01 01:12:23 -0300

    adding cors middleware

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const mailRouter = require("./src/routes/mail.js");
+const cors = require("cors");
 const app = express();
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
