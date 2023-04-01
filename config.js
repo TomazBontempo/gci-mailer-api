a4276e92965e667d2375dd25040813e7b8b22ac4
Tomaz Bontempo <tomaz.bontempo@gmail.com>
2023-03-31 22:27:00 -0300

    updating access keys

---
+++ config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  user: "guiadoconsumidorinteligente@gmail.com",
+  clientId:
+    "992327083265-n5rdb0nmta7jbttioe5f07lgtgt0tk1v.apps.googleusercontent.com",
+  clientSecret: "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",
+  refreshToken:
+    "1//04oo8YGqSUfoICgYIARAAGAQSNwF-L9IrLXg4JbxhPx5QQDy8uMIRssnG0GDnfzNbQSz6M8fvuxGkzuzLd3BKDkEoifqSYBnZfN0",
+};
